09e19e233a3d55d3d869b74222da11b172a96918
Jueee <hellojue@foxmail.com>
2015-11-30 09:31:20 +0800

    add No.2

---
+++ BasicBlog/www/app.py
@@ -0,0 +1,24 @@
+
+# 20151130-02
+# 由于我们的Web App建立在asyncio的基础上，因此用aiohttp写一个基本的app.py：
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv = yield from loop.create_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
